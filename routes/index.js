d0fc9926c0b74b54b17886e0dea92ed2a83faeaa
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-06-13 20:09:20 -0700

    pass list of jobs to index route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+let models  = require('../models');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  models.Job.findAll().then(function (results) {
+    res.render('index', {
+      jobs: results
+    });
+  });
 });
 
 module.exports = router;
