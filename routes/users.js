faae8722c06da54276e8fe80358a6479914476fa
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-06-15 19:23:06 -0700

    add user login route

--- routes/users.js
+++ routes/users.js
@@ -10,4 +10,8 @@ router.get('/register', function(req, res, next) {
   res.render('user-register');
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('user-login');
+});
+
 module.exports = router;
