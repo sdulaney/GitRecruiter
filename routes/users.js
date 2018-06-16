d2514d3e9825c70041dfd4f156b60625c846cb2f
Stewart Dulaney <sdulaney@users.noreply.github.com>
2018-06-15 19:19:39 -0700

    add user registration route

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/register', function(req, res, next) {
+  res.render('user-register');
+});
+
 module.exports = router;
